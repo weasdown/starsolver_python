7f6918e51f704f84e77ef53a722451e0ffffe14c
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-04 18:57:07 +0000

    Add placeholder test_starsolver.py

---
+++ test_starsolver.py
@@ -0,0 +1,13 @@
+import unittest
+import starsolver
+
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_absolute_truth_and_meaning(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
