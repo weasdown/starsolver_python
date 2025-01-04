b11d24228f960da35a8f814e9c07198a488aaa59
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-04 17:25:30 +0000

    Add CellStatus enum, add docstrings

--- cell.py
+++ cell.py
@@ -1,10 +1,18 @@
-# Cell class
+# Cell library
+
+from enum import Enum
 
 class Cell:
     def __init__(self, coords: tuple):
+        """A single cell in a board."""
         self.coords = coords
         self.row = self.coords[0]
         self.column = self.coords[1]
 
         self.status = ...  # TODO empty, dotted or starred
 
+class CellStatus(Enum):
+    """The three possible statuses for a Cell."""
+    blank = 0
+    dot = 1
+    star = 2
