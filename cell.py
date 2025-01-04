a6430e9cf5efa780062e3b7f5a7e377dfa70ce32
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-04 18:12:49 +0000

    Add status param to Cell.__init__(), update .idea files

--- cell.py
+++ cell.py
@@ -2,17 +2,18 @@
 
 from enum import Enum
 
-class Cell:
-    def __init__(self, coords: tuple):
-        """A single cell in a board."""
-        self.coords = coords
-        self.row = self.coords[0]
-        self.column = self.coords[1]
-
-        self.status = ...  # TODO empty, dotted or starred
-
 class CellStatus(Enum):
     """The three possible statuses for a Cell."""
     blank = 0
     dot = 1
     star = 2
+
+
+class Cell:
+    def __init__(self, coords: tuple, status: CellStatus = CellStatus.blank):
+        """A single cell in a board."""
+        self.coords = coords
+        self.row = self.coords[0]
+        self.column = self.coords[1]
+
+        self.status: CellStatus = status
