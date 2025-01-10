780fc5e7a9a4ca5531d96d50a03348818bd534d5
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-10 09:01:40 +0000

    Add board property prints

--- example.py
+++ example.py
@@ -2,6 +2,17 @@
 
 import starsolver as s
 
-board = s.Board()
+board: s.Board = s.Board()
 
+print(f'{board.num_blanks = }')
+print(f'{board.num_dots = }')
+print(f'{board.num_stars = }\n')
+
+board[0][0].dot()
+print(board[0][0])
+
+print(f'\n{board.num_blanks = }')
+print(f'{board.num_dots = }')
 print(f'{board.num_stars = }')
+
+print(f'\n{board[0].cells}')
