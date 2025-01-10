32372c0c0fc482a7efecd52ce4601f5a28490bde
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-10 10:08:35 +0000

    Add board print and text formatting examples

--- example.py
+++ example.py
@@ -16,3 +16,26 @@ print(f'{board.num_dots = }')
 print(f'{board.num_stars = }')
 
 print(f'\n{board[0].cells}')
+
+print(f'Full board:\n'
+      f'{board}')
+
+print('\nFirst row of board:\n'
+      f'{board.rows[0].board_print()}')
+
+
+class color:
+    """Copied from https://stackoverflow.com/questions/8924173/how-can-i-print-bold-text-in-python"""
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+print(color.BOLD + 'Hello, World!' + color.END)
