6d09ac97f4eae16aacb6d5f8eebec5f28573d337
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-10 09:00:45 +0000

    Add Board.__getitem__(), build_shapes(), cell_from_coord(), num_blanks, num_dots, add Column/Row.__getitem__()

--- starsolver/board.py
+++ starsolver/board.py
@@ -1,52 +1,98 @@
 # Board library
 
-from starsolver.cell import Cell, CellStatus
-from .shape import Shape
+from __future__ import annotations
 
-import numpy as np
+import starsolver.cell as c
+import starsolver.cell_group as cg
+import starsolver.shape as s
+
 
 class Board:
-    dimension: int = 9  # Number of Cells on a side of the Board
+    dimension: int = 9  # Number of cells on a side of the Board
 
-    def __init__(self):
+    def __init__(self, shapes: list[dict[str, list[c.Coordinate] | int]] = None):
         """A 9x9 board in which a puzzle takes place."""
-        self.rows: list[Row] = [Row(index) for index in  range(self.dimension)]
-        self.columns: list[Column] = [Column(index) for index in  range(self.dimension)]
+        self.rows: list[Row] = [Row(index) for index in range(self.dimension)]
+        self.columns: list[Column] = [Column(index) for index in range(self.dimension)]
 
-        self.shapes: list[Shape] = ...  # TODO implement shapes
+        """A list of all the Rows in the Board."""
+        self.rows: list[cg.Row] = [cg.Row(index) for index in range(Board.dimension)]
 
-        self.cells: list[Cell] = [cell for row in self.rows for cell in row.cells]
+        """A list of all the Columns in the Board."""
+        self.columns: list[cg.Column] = [cg.Column(index) for index in range(Board.dimension)]
+
+        """A list of all the Cells in the Board."""
+        self.cells: list[c.Cell] = [cell for row in self.rows for cell in row.cells]
+
+        """A list of all the Shapes in the Board."""
+        self.shapes: list[s.Shape] = self.build_shapes(shapes)
+
+    def __getitem__(self, index: int):
+        return self.rows[index]
+
+    @staticmethod
+    def build_shapes(shapes: list[dict[str, list[c.Coordinate] | int]]):
+        if shapes is None:
+            return []
+        else:
+            return [s.Shape(index, shape_dict['coords'], shape_dict['colour'])
+                    for (index, shape_dict) in enumerate(shapes)]
+
+    def cell_from_coord(self, coord: c.Coordinate) -> c.Cell:
+        return [cell for cell in self.cells if (cell.coord == coord)][0]
 
     @property
     def is_complete(self) -> bool:
         """Gets whether the Board has been completed and is valid."""
         raise NotImplementedError
-    
+
     @property
     def is_valid(self) -> bool:
         """Gets whether the Board is valid."""
         raise NotImplementedError
 
     @property
+    def num_blanks(self) -> int:
+        """Gets how many blank cells are in the Board."""
+        blank_cells: list[c.Cell] = [cell for cell in self.cells if cell.status == c.CellStatus.blank]
+        return len(blank_cells)
+
+    @property
+    def num_dots(self) -> int:
+        """Gets how many dotted cells are in the Board."""
+        dotted_cells: list[c.Cell] = [cell for cell in self.cells if cell.status == c.CellStatus.dot]
+        return len(dotted_cells)
+
+    @property
     def num_stars(self) -> int:
         """Gets how many stars are in the Board."""
-        starred_cells: list[Cell] = [cell for cell in self.cells if cell.status == CellStatus.star]
+        starred_cells: list[c.Cell] = [cell for cell in self.cells if cell.status == c.CellStatus.star]
         return len(starred_cells)
+
 
 class Column:
     def __init__(self, index: int):
         self.index: int = index
-        
-        self.cells: list[Cell] = [Cell((self.index, vert_index)) for vert_index in range(Board.dimension)]
+
+        self.cells: list[c.Cell] = [c.Cell(c.Coordinate(self.index, vert_index))
+                                    for vert_index in range(Board.dimension)]
+
+    def __getitem__(self, index: int) -> c.Cell:
+        return self.cells[index]
 
     def __repr__(self):
         return f'Column({self.index})'
 
+
 class Row:
     def __init__(self, index: int):
         self.index: int = index
-        
-        self.cells: list[Cell] = [Cell((horiz_index, self.index)) for horiz_index in range(Board.dimension)]
+
+        self.cells: list[c.Cell] = [c.Cell(c.Coordinate(horiz_index, self.index)) for horiz_index in
+                                    range(Board.dimension)]
+
+    def __getitem__(self, index: int) -> c.Cell:
+        return self.cells[index]
 
     def __repr__(self):
         return f'Row({self.index})'
