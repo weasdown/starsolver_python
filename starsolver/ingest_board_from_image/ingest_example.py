63159b7bd85ec73d845aabbcddc194fd66b4ebef
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-19 14:19:33 +0000

    feat: implement example

--- starsolver/ingest_board_from_image/ingest_example.py
+++ starsolver/ingest_board_from_image/ingest_example.py
@@ -1,6 +1,10 @@
 # Example of creating a Board instance from an image of a board.
 
+import ingest
 import starsolver as s
 
-b: s.Board = s.Board()
+samples_path: str = 'samples'
+sample_image_name: str = 'sample_complete.png'
+
+b: s.Board = ingest.ingest(image_path=f'{samples_path}/{sample_image_name}')
 print(b)
