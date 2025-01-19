9fe0a70aef69103e66822cda8af4342eae3845fe
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-19 22:42:26 +0000

    feat: Add sample_partial.png

--- starsolver/ingest_board_from_image/ingest_example.py
+++ starsolver/ingest_board_from_image/ingest_example.py
@@ -7,7 +7,7 @@ import starsolver as s
 
 # Assume the samples folder is in the same directory as this file.
 samples_path: str = f'{os.path.dirname(os.path.realpath(__file__))}/samples'
-sample_image_name: str = 'sample_complete.png'
+sample_image_name: str = 'sample_partial.png'
 
 b: s.Board = ingest.ingest(image_path=f'{samples_path}/{sample_image_name}')
 print(b)
