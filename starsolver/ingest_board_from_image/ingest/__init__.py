8cbebcb895c1505d8da62d6b463fd7f3acac2db0
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-19 14:19:28 +0000

    feat: add ingest.py with placeholder ingest function

--- starsolver/ingest_board_from_image/ingest/__init__.py
+++ starsolver/ingest_board_from_image/ingest/__init__.py
@@ -1,1 +1,3 @@
 # File to make this directory a valid module in its own right.
+
+from .ingest import ingest
---
+++ starsolver/ingest_board_from_image/ingest/ingest.py
@@ -0,0 +1,17 @@
+# Builds a Board instance from an image of a board.
+
+import starsolver as s
+
+
+def ingest(image_path: str) -> s.Board:
+    """
+    Builds a Board instance from an image of a board.
+
+    :param image_path: The path to an image of the required board.
+    :return: A Board instance matching the board in the input image.
+    """
+
+    # TODO implement ingest
+
+    b: s.Board = s.Board()
+    return b
