7dbcfa6611ee807942e1bd0719466a8e41857235
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-19 14:29:18 +0000

    fix: fix samples_path

--- starsolver/ingest_board_from_image/ingest_example.py
+++ starsolver/ingest_board_from_image/ingest_example.py
@@ -1,9 +1,12 @@
 # Example of creating a Board instance from an image of a board.
 
+import os
+
 import ingest
 import starsolver as s
 
-samples_path: str = 'samples'
+# Assume the samples folder is in the same directory as this file.
+samples_path: str = f'{os.path.dirname(os.path.realpath(__file__))}/samples'
 sample_image_name: str = 'sample_complete.png'
 
 b: s.Board = ingest.ingest(image_path=f'{samples_path}/{sample_image_name}')
