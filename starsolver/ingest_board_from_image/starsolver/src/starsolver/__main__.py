3f14cea70d79dc141f80a0530362f413e84159b3
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-20 12:32:01 +0000

    feat: Add briefcase example app called starsolver

---
+++ starsolver/ingest_board_from_image/starsolver/src/starsolver/__main__.py
@@ -0,0 +1,5 @@
+from starsolver.app import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ starsolver/ingest_board_from_image/starsolver/src/starsolver/app.py
@@ -0,0 +1,10 @@
+"""
+Solver for Star Battle/Two Not Touch puzzles
+"""
+
+
+def main():
+    # This should start and launch your app!
+    image_path: str = r'C:\Users\weasd\dev\non-OH\starsolver_python\starsolver\ingest_board_from_image\samples\sample_partial.png'
+    print(f'You want to process an image at path "{image_path}"')
+    pass
