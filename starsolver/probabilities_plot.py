5018bee4076ad7f8d88b749c79b6ca08bc725024
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-19 09:39:40 +0100

    feat: add probabilities_plot.py

---
+++ starsolver/probabilities_plot.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+
+
+def plot() -> None:
+    """Plots a 3D surface plot of the probability that each cell in an array will have a star."""
+    raise NotImplementedError('plot() is not yet implemented.')
